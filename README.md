10a059fcec5441df139a69336240f798628821ba
unknown <rainmancumt@163.com>
2022-12-09 22:27:30 +0800

    V2.1.2 published

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Optimze And Combine CSS &amp;&amp; JavaScript
 
 ## 简介
 
-用于实现PHP项目中的前端优化，包括CSS、JS文件的压缩和合并。
+用于合并项目中的css和js文件，并对文件进行混淆和压缩。
 
 ## 安装
 将php加入环境变量，并在命令行中执行
@@ -14,18 +14,45 @@ Optimze And Combine CSS &amp;&amp; JavaScript
 
 ## 功能概述
 
-* 压缩项目中所有.css和.js文件;
-* 将项目中所有xx.js.php和xx.css.php执行后输出为xx.c.js和xx.c.js，并删除xx.js.php和xx.css.php文件
-* 将项目中所有对"xx.js.php"和"xx.css.php"引用替换为"xx.c.js"和"xx.c.js"，例如 index.html
 
-        <script type="text/javascript" src="./xx.js.php"></script>
-        替换为：
-        <script type="text/javascript" src="./xx.c.js"></script>
+
+smooth.config.js
+
+
+    const path = require('path');
+    
+    module.exports = {
+      mode: 'development',  // production
+      path: __dirname,
+      copy: {
+        source: path.resolve(__dirname, './src'),
+        dest: path.resolve(__dirname, './dist'),
+        mode: 'development',
+        removeCombineFiles: true
+      },
+      obfuscate: {
+        compact: false,
+        controlFlowFlattening: true
+      },
+      js: {
+        'admin': {
+          mode: 'production',
+          output: './dist/admin.js',
+          obfuscate: true,
+          input: [
+            './src/a.js',
+            './src/b.js',
+          ]
+        }
+      },
+      css: {}, 
+    };
+
 
 ## 使用说明
 
 在项目录下执行
 	
-	smooth	../output
+	smooth
 
-此时会在项目的同级目录下生成output文件夹，将该文件夹拷贝至服务器中即可。
+此时会在项目的同级目录下生成dist文件夹，将该文件夹拷贝至服务器中即可。
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /***********************************************************************
  1. update \uglify-js\lib\parse.js    增加filename属性
-    
-    function JS_Parse_Error(message, line, col, pos, filename) {
+
+ function JS_Parse_Error(message, line, col, pos, filename) {
         this.message = message;
         this.line = line;
         this.col = col;
@@ -10,81 +10,99 @@
         this.filename = filename;
     };
 
-    function js_error(message, filename, line, col, pos) {
+ function js_error(message, filename, line, col, pos) {
         throw new JS_Parse_Error(message, line, col, pos, filename);
     };
 
-***********************************************************************/
+ ***********************************************************************/
 
 require('./lib/smooth');
 
-var VERSION = require('./package.json')['version'];
+const VERSION = require('./package.json')['version'];
+const Path = require('path');
+const util = require('./lib/util');
+const t0 = new Date();
 
-var pth = require('path'),
-    util = require('./lib/util'),
-    once = false,
-    t0 = new Date();
+const run = function (args) {
+    // 基础路径
+    let pathNode = args[0];         // node.exe的完整路径
+    let pathNpm = args[1];          // index.js的完整路径
+    let pathCwd = process.cwd();    // 命令执行时所在的路径
 
-function normalizePath(path, cwd) {
-    if (path[0] !== '/' && path[0] !== '\\' && path.indexOf(':') < 0) {
-        path = pth.normalize(cwd + '/' + path);
-    }
-    if (path[path.length - 1] === '/' || path[path.length - 1] === '\\') {
-        path = path.substr(0, path.length - 1);
-    }
-    return path;
-}
-
-var run = function(args) {
-    if (!once) {
-        once = true;
-    } else {
+    // 计算smooth.config.js文件路径
+    let pathConfig = args[2];  // smooth.config.js的路径    
+    if (pathConfig === '-v') {
+        smooth.log.warn('Smooth v' + VERSION);
         return;
     }
-
-    var len = args.length,
-        cwd = process.cwd(),
-        src = cwd,
-        dst;
-
-    if (len == 3) {
-        dst = args[2];
-    } else if (len == 4) {
-        src = args[2];
-        dst = args[3];
+    if (args.length === 2) {
+        pathConfig = './smooth.config.js';
     }
-    if (len === 3 || len === 4) {
-        dst = normalizePath(dst, cwd);
-        src = normalizePath(src, cwd);
-        if (!util.exists(src)) {
-            smooth.log.error('Error Path Not Exist: ' + src);
-            return;
+    pathConfig = Path.resolve(pathCwd, pathConfig);  // __dirname 代表 index.js文件所在的目录  // path_config = pth.resolve(__dirname, path_config);  // __dirname 代表 index.js文件所在的目录
+    if (!util.exists(pathConfig)) {
+        smooth.log.error('File Not Exist: ' + pathConfig);
+        return;
+    }
+    // 加载基本配置
+    let smooth_config = require(pathConfig);
+    let baseMode = smooth_config['mode'];
+    let baseDir = smooth_config['path'];
+    let jsItems = smooth_config['js'];
+    let cssItems = smooth_config['css'];
+    let copyItems = smooth_config['copy'];
+    let obfConfig = smooth_config['obfuscate'];
+
+    // 合并js
+    if (jsItems) {
+        for (let p in jsItems) {
+            let {output, input, mode, obfuscate} = jsItems[p];
+            let result = smooth.combineJS(output, input, baseDir, mode ? mode : baseMode, obfuscate, obfConfig);
+            if (result === false) return;
         }
-        if (util.startWith(dst, pth.normalize(util.endWith(src, pth.sep) ? src: src + pth.sep))) {
-            smooth.log.error('Error Output Path: ' + dst);
-            return;
+        smooth.log.flag('Combine JS');
+    }
+    // 合并css
+    if (cssItems) {
+        for (let p in cssItems) {
+            let {output, input, mode} = cssItems[p];
+            let result = smooth.combineCSS(output, input, baseDir, mode ? mode : baseMode);
+            if (result === false) return;
         }
-        
-        smooth.init(src, dst);
-        smooth.log.flag('init');
-        if (smooth.config.get('optimize')) {
-            smooth.optimize();
+        smooth.log.flag('Combine CSS');
+    }
+    if (copyItems) {
+        let {source, dest, mode, removeCombineFiles} = copyItems;
+        // 拷贝目录和文件
+        util.copy(source, dest);
+        smooth.log.flag('Copy Files');
+        // 删除合并的源文件
+        if (removeCombineFiles) {
+            if (jsItems) {
+                for (let p in jsItems) {
+                    jsItems[p]['input'].forEach(function (file) {
+                        util.del(dest + Path.resolve(baseDir, file).replace(source, ''));
+                    });
+                }
+            }
+            // 合并css
+            if (cssItems) {
+                for (let p in cssItems) {
+                    cssItems[p]['input'].forEach(function (file) {
+                        util.del(dest + Path.resolve(baseDir, file).replace(source, ''));
+                    });
+                }
+            }
+            smooth.log.flag('Delete Combine Source Files');
         }
-        if (smooth.config.get('combine')) {
-            smooth.combine();
-            smooth.log.flag('combine');
-        }
-    } else {
-        smooth.log.warn('Smooth v' + VERSION);
     }
 };
 
-if (process.argv.length === 4) {
-    run(process.argv);
-}
+//run(process.argv);    // 开发时打开此行代码
 
-process.on('exit', function() {
+
+process.on('exit', function () {
     smooth.log.notice('Used ' + (new Date() - t0) + 'ms');
 });
+
 
 exports.run = run;
